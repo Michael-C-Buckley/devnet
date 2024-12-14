b01bea38ad66c2efd0f8e63e451ac8a24e616a49
Michael Buckley <michaelcbuckley@proton.me>
2024-12-14 11:49:55 -0500

    Created structure and added learning objectives

---
+++ catalyst/restconf.py
@@ -0,0 +1,4 @@
+"""
+3.3 Configure device using RESTCONF API utilizing Python requests library
+3.5 Configure a subscription for model driven telemetry on an IOS XE device (CLI, NETCONF, and RESTCONF)
+"""
---
+++ sdwan/sdwan_api.py
@@ -0,0 +1,7 @@
+"""
+5.2 Implement a Python script to perform API requests for Cisco SD-WAN vManage Device Inventory APIs to retrieve and display data
+5.3 Construct API requests for Cisco SD-WAN vManage Administration APIs
+5.4 Implement a Python script to perform API requests for Cisco SD-WAN vManage Configuration APIs to modify Cisco SD-WAN fabric configuration
+5.5 Construct API requests for Cisco SD-WAN vManage Monitoring APIs (including real-time)
+5.6 Troubleshoot a Cisco SD-WAN deployment using vManage APIs
+"""
