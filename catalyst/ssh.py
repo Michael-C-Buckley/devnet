ac23d5b5b926c1e608a49c802590d618fbad7750
Michael Buckley <michaelcbuckley@proton.me>
2024-12-14 15:21:24 -0500

    Added simple SSH script

---
+++ catalyst/ssh.py
@@ -0,0 +1,33 @@
+"""
+3.1 Implement device management and monitoring using Netmiko
+"""
+
+# Python Modules
+from ipaddress import IPv4Address as IPv4, IPv6Address as IPv6
+from re import search
+
+# Third-Party Modules
+from netmiko import BaseConnection, ConnectHandler
+
+type HostT = IPv4 | IPv6 | str
+
+
+def connect_device(host: HostT, port: int, username: str, password: str) -> BaseConnection:
+    input_kwargs = {k: v for k, v in locals().items()}
+    device = ConnectHandler(**input_kwargs, device_type='cisco_ios')
+    return device
+
+
+def add_loopback(loopback_id: int, host: HostT, port: int, username: str, password: str) -> bool:
+    config = [
+        f'int loopback{loopback_id}',
+        'ip address 10.101.1.101 255.255.255.255',
+        'no shut',
+    ]
+    device = connect_device(host, port, username, password)
+    device.enable()
+    device.send_config_set(config)
+
+    response = device.send_command('show ip interface brief')
+
+    return bool(search(f'loopback{loopback_id}', response))
